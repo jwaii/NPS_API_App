7d7c3a7a607b33d18a9d5a694d0d8fb6ed1f7bf8
Justin Wai <justin.wai.97@gmail.com>
2019-12-02 15:21:55 -0800

    update

---
+++ app.js
@@ -0,0 +1,88 @@
+'use strict'
+
+//practice leaving notes for self
+
+const apiKey = 'KYMawks2jpDel05cxQgHKKH9OmWfhAxj4yQ9nGrr';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+// https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=KYMawks2jpDel05cxQgHKKH9OmWfhAxj4yQ9nGrr
+// developer.nps.gov/api/v1
+
+console.log('api keys working');
+
+
+
+function formatQueryParams(params){
+    console.log('format Q params ran')
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        console.log(queryItems) //call for the keys
+        return queryItems.join('&');
+}
+
+/*
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&'); // .join will join values by turing value into strings, EX: value1&value2&value3
+  }
+  */
+
+function formatApiUrl(state, limit=10){
+    const params = {
+        key: apiKey,
+        stateCode: state,
+        maxResults: limit,
+    }
+    //console.log('params')
+    const queryStrings = formatQueryParams(params);
+   // console.log(queryStrings);
+    const Url = searchURL+ '?' + queryStrings
+    console.log(Url);
+
+    fetch(Url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.status.Text);
+        })
+        .then(reponseJson => displayResults(responseJson))
+        .catch(err => {
+            $('js-error-message').text(`Something went wronf: ${err.message}`);
+        });
+}
+
+
+
+function getParkResults(){
+
+
+}
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchPark = $('#js-search-park').val();
+        const maxResults = $('#js-max-results').val();
+        console.log(searchPark, maxResults);
+        console.log('watch form working');
+        formatApiUrl(searchPark,maxResults);
+        // function to get NPS Results
+    })
+}
+
+$(watchForm);
+
+
+//order 
+    //user form
+    //api
+    //show to user
+
+
+    //url built line 39 "const Url = searchURL+ '?' + queryStrings"
+    //get park results takes 1 argument (Url)  X
+        //Fetch will live here  X
+            //return response in Jsonform  
+            //display results :)
